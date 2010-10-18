9d24ef6c80d152ec0cbcc7dce22b0765837fb0a3
Vlad Grigorescu <grigorescu@gmail.com>
2010-10-17 21:53:37 -0500

    Extra files removed.

--- trunk/pygminion/cards/simpleCallbacks.py
+++
@@ -1,5 +0,0 @@
-def add(gameState, actions=0, cards=0, money=0, points=0):
-    gameState.players[gameState.currentPlayer].actions += actions
-    gameState.players[gameState.currentPlayer].draw(cards)
-    gameState.players[gameState.currentPlayer].actions += money
-    gameState.players[gameState.currentPlayer].actions += points
